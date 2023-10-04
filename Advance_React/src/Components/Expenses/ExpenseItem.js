31820c63e0f88190664bee61d40510d41edc0d73
Thaheershaik6 <thaheershaik866@gmail.com>
2023-10-04 21:41:26 +0530

    Task-1 Adding Event Listeners made easy added

---
+++ Advance_React/src/Components/Expenses/ExpenseItem.js
@@ -0,0 +1,19 @@
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+const deletebutton = ()=>{
+  console.log('Expens Deleted')
+}
+  return (
+    <Card className='expense-item'>
+    <ExpenseDate date={props.date} />
+    <ExpenseDetails amount={props.amount} location={props.location}  title={props.title} />
+    <button>Delete Expense</button>
+    </Card> 
+  );
+}
+
+export default ExpenseItem;
