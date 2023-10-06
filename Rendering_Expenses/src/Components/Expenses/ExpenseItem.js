aa07417eda916583486d599cd3f71d1d596b10c5
Thaheershaik6 <thaheershaik866@gmail.com>
2023-10-06 13:09:52 +0530

    Task-10 Rendering Expenses with performance added

---
+++ Rendering_Expenses/src/Components/Expenses/ExpenseItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetails from "./ExpenseDetails";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  return (
+    <div>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <ExpenseDetails
+          amount={props.amount}
+          location={props.location}
+          title={props.title}
+        />
+      </Card>
+    </div>
+  );
+};
+
+export default ExpenseItem;
